dd216a5f0c80d44bc4a669ab6c9b0149f7c98fa6
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-04-19 20:18:18 -0500

    hello world

---
+++ HelloCommand.php
@@ -0,0 +1,20 @@
+mespace JoshuaEstes\stHelloWorld;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloCommand extends Command
+{
+  protected function configure()
+  {
+    $this->setName('helloworld:hello');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $output->writeln("WORLD!");
+  }
+}
